ea3ede70c800f7063620ea9243ed79ebc6a72bc2
douglasvinicio <douglas.vinicio@hotmail.com>
2021-01-15 18:49:02 -0800

    Trips First Commit

---
+++ Trips/Trips/Travel.xaml.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Trips
+{
+    public partial class MainWindow : Window
+    {
+        const string DataFileName = @"..\..\trips.txt";
+        static List<Trip> TripList = new List<Trip>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadDataFromFile();
+        }
+
+        private void LoadDataFromFile()
+        {
+            if (File.Exists(DataFileName))
+            {
+                try
+                {
+                    string[] linesArray = File.ReadAllLines(DataFileName);
+                    foreach (string line in linesArray)
+                    {
+                        try
+                        {
+                            Trip trip = new Trip(line);
+                            TripList.Add(trip);
+                        }
+                        catch (InvalidDataException ex)
+                        {
+                            Console.WriteLine("Error: " + ex.Message);
+                        }
+                    }
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine("Error reading from file: " + ex.Message);
+                }
+
+            }
+            //Populating list view with file data
+            lvTripList.ItemsSource = TripList;
+        }
+
+        private void btnAddTrip_Click(object sender, RoutedEventArgs e)
+        {
+            string destination = txtDestination.Text;
+            string name = txtName.Text;
+            string passport = txtPassport.Text;
+            string departureDate = dpDepartureDate.Text;
+            string returnDate = dpReturnDate.Text;
+
+            Trip trip = new Trip(destination,name,passport,departureDate,returnDate);
+            TripList.Add(trip);
+
+            ResetValues();
+        }
+
+        private void lvTripList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Activating buttons when Item in the list is selected
+            btnUpdateTrip.IsEnabled = true;
+            btnDeleteTrip.IsEnabled = true;
+
+
+            var selectedTrip = lvTripList.SelectedItem;
+
+            if (selectedTrip is Trip)
+            {
+                Trip trip = (Trip)lvTripList.SelectedItem;
+
+
+                // Assigning values as an object inside the application
+                txtDestination.Text = trip.Destination;
+                txtName.Text = trip.Name;
+                txtPassport.Text = trip.Passport;
+                dpDepartureDate.Text = trip.DepartureDate;
+                dpReturnDate.Text = trip.ReturnDate;
+            }
+        }
+
+        public void SaveFile()
+        {
+            using (StreamWriter writer = new StreamWriter(DataFileName))
+            {
+                foreach (Trip trip in TripList)
+                {
+                    writer.WriteLine(trip.ToDataString());
+                }
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void btnUpdateTrip_Click(object sender, RoutedEventArgs e)
+        {
+            string newDestination = txtDestination.Text;
+            string newName = txtName.Text;
+            string newPassport = txtPassport.Text;
+            string newDepartureDate = dpDepartureDate.Text;
+            string newReturnDate = dpReturnDate.Text;
+
+            Trip tripUpdate = (Trip)lvTripList.SelectedItem;
+
+            tripUpdate.Destination = newDestination;
+            tripUpdate.Name = newName;
+            tripUpdate.Passport = newPassport;
+            tripUpdate.DepartureDate = newDepartureDate;
+            tripUpdate.ReturnDate = newReturnDate;
+
+            ResetValues();
+        }
+
+        private void ResetValues()
+        {
+            lvTripList.Items.Refresh();
+            txtDestination.Clear();
+            txtName.Clear();
+            txtPassport.Clear();
+            dpDepartureDate.Text = "";
+            dpReturnDate.Text = "";
+            lvTripList.SelectedIndex = -1;
+            btnUpdateTrip.IsEnabled = false;
+            btnDeleteTrip.IsEnabled = false;
+        }
+
+        private void btnDeleteTrip_Click(object sender, RoutedEventArgs e)
+        {
+            Trip tripDelete = (Trip)lvTripList.SelectedItem;
+            TripList.Remove(tripDelete);
+
+            ResetValues();
+        }
+
+        private void btnSaveSelected_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Trip file (*.trip)|*.trip";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                using (StreamWriter writer = new StreamWriter(saveFileDialog.FileName))
+                {
+                    foreach (Trip trip in TripList)
+                    {
+                        writer.WriteLine(trip.ToDataString());
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Trips/Trips/Trip.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Trips
+{
+    class Trip
+    {
+        public class InvalidDataException : Exception
+        {
+            public InvalidDataException(string msg) : base(msg) { }
+        }
+
+        // Constructors
+        public Trip(string destination, string name, string passport, string departureDate, string returnDate)
+        {
+            Destination = destination;
+            Name = name;
+            Passport = passport;
+            DepartureDate = departureDate;
+            ReturnDate = returnDate;
+        }
+        public Trip(string dataLine)
+        {
+            string[] data = dataLine.Split(';');
+            if (data.Length != 5)
+            {
+                throw new InvalidDataException("Line has invalid number for fields:\n" + dataLine);
+            }
+            Destination = data[0];
+            Name = data[1];
+            Passport = data[2];
+            DepartureDate = data[3];
+            ReturnDate = data[4];
+        }
+
+        // Getters and Setters
+        private string _destination;
+        public string Destination
+        {
+            get
+            {
+                return _destination;
+            }
+            set
+            {
+                if (value.Contains(";"))
+                {
+                    throw new InvalidDataException("It contains a invalid character ';'. Please remove it and try it again.");
+                }
+                _destination = value;
+            }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value.Contains(";"))
+                {
+                    throw new InvalidDataException("It contains a invalid character ';'. Please remove it and try it again.");
+                }
+                _name = value;
+            }
+        }
+
+        private string _passport;
+        public string Passport 
+        {
+            get
+            {
+                return _passport;
+            }
+            set
+            {
+                string pattern = @"^[A-Za-z 0-9,\-.]{1,20}$";
+                Regex rg = new Regex(pattern);
+                if (!rg.IsMatch(value))
+                {
+                    throw new InvalidDataException("Passport must have between 1 to 20 characters and no special characters.");
+                }
+                _passport = value;
+            }
+        }
+
+        private string _departureDate;
+        public string DepartureDate { get; set; }
+
+        private string _returnDate;
+        public string ReturnDate { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0};{1};{2};{3};{4}", Destination, Name, Passport, DepartureDate, ReturnDate); // testing purposes
+        }
+
+        public virtual string ToDataString()
+        {
+            return string.Format("{0};{1};{2};{3};{4}", Destination, Name, Passport, DepartureDate, ReturnDate);
+        }
+    }
+}
